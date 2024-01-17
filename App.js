b0215a00fce9322e0da0b45d4e681c795576fbf6
avegap23 <avpampliega@gmail.com>
2024-01-17 10:47:45 +0100

    newGoal

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, TextInput, Button, Text, StyleSheet } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+
+  //declaramos el hook de estado de componente "newGoal"
+  const [newGoal, setNewGoal] = useState("");
+
+  function textChangeHandler(enteredText) {
+    setNewGoal(enteredText);
+    console.log(newGoal);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput placeholder='Input your goal'
+          style={styles.textInput}
+          onChangeText={textChangeHandler} />
+        <Button title='Add goal' />
+      </View>
+      <View style={styles.goalsContainer}>
+        <Text>Your list of goals...</Text>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const styles = new StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 30,
+    paddingHorizontal: 20
   },
-});
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: '#CCCCCC',
+    alignItems: 'center'
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#CCCCCC',
+    width: "70%",
+    borderBottomWidth: 1,
+    padding: 7
+  },
+  goalsContainer: {
+    flex: 5
+  }
+})
